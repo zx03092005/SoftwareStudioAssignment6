02d4f7a964e763132f13794d4e7e2e42bb79d0c7
Wingsyu <zx03092005@hotmail.com>
2016-05-08 18:44:21 +0800

    add position x y decode color add method addtarget

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 /**
@@ -11,17 +13,43 @@ public class Character {
 	private MainApplet parent;
 	private String name;
 	private String colour;
-	
+	private int color;
+	public float x, y;
+	private float startX, startY;
+	private boolean inCircle;
+	private ArrayList<Character> targets = new ArrayList<Character>();
+	private ArrayList<Integer> values= new ArrayList<Integer>();
 
-	public Character(MainApplet parent, String name, String colour){
+	public Character(MainApplet parent, String name, String colour, float x, float y){
 
 		this.parent = parent;
 		this.name = name;
 		this.colour = colour;
+		this.color = this.parent.unhex(colour.substring(1));
+		
+		this.x = this.startX = x;
+		this.y = this.startY = y;
+		this.inCircle = false;
 	}
 
 	public void display(){
 
 	}
 	
+	public void addTarget(Character target, Integer value) {
+		this.targets.add(target);
+		this.values.add(value);
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public float getStartX() {
+		return this.startX;
+	}
+	
+	public float getStartY() {
+		return this.startY;
+	}
 }
